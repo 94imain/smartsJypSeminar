ef120c106d674425488a3e1ec7d7d7cd5b295d1e
kimwoojin <94rladnwls@naver.com>
2017-08-25 16:44:16 +0900

    progressbar status binding

--- app/src/main/java/com/example/woojinkim/databindingpractice/MainActivity.java
+++ app/src/main/java/com/example/woojinkim/databindingpractice/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.woojinkim.databindingpractice;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.woojinkim.databindingpractice.databinding.ActivityMainBinding;
+import com.example.woojinkim.databindingpractice.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        MainViewModel mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        binding.setMainviewmodel(mainViewModel);
+
     }
 }
--- app/src/main/java/com/example/woojinkim/databindingpractice/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/woojinkim/databindingpractice/viewmodel/MainViewModel.java
@@ -1,8 +1,19 @@
 package com.example.woojinkim.databindingpractice.viewmodel;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+import android.support.annotation.NonNull;
+import android.view.View;
+
 /**
  * Created by woojinkim on 2017. 8. 25..
  */
 
-public class MainViewModel {
+public class MainViewModel extends ViewModel {
+    @NonNull
+    public ObservableField<String> mTitle = new ObservableField<>("initial");
+
+    public ObservableField<Integer> mProgressbarStatus = new ObservableField<>(View.GONE);
+
 }
